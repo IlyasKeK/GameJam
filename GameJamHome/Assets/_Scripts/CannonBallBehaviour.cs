204086957e0d3821aa14bb2d72973e7e824fe010
Ilyas <issatayev.ilyas@gmail.com>
2019-01-25 23:37:39 +0100

    Added test behaviour for cannonball

---
+++ GameJamHome/Assets/_Scripts/CannonBallBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallBehaviour : MonoBehaviour {
+    [SerializeField]
+    private Vector2 m_initialForce=new Vector2(1,-1);
+
+    private Rigidbody2D m_rigidbody2D;
+
+	void Start () {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_rigidbody2D.velocity = m_initialForce;
+	}
+	
+	void Update () {
+		
+	}
+}
