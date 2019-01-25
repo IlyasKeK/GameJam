9554aff439a619b11dd1637d67676949c86cfca5
Unknown <Coen_helthuis@hotmail.com>
2019-01-25 23:52:48 +0100

    beautiful main menu, rly
    Play(Load Scene)
    Quit (Quit application)

---
+++ GameJamHome/Assets/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    [SerializeField]
+    private string _levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Play() {
+        SceneManager.LoadScene(_levelToLoad);
+        return;
+        if (Application.CanStreamedLevelBeLoaded(_levelToLoad))
+        {
+            SceneManager.LoadScene(_levelToLoad);
+        }
+        else
+        {
+            Debug.Log("this level does not exist");
+        }
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+}
