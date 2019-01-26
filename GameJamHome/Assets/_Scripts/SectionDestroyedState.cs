6dd59df4db6ac2425e6b1e86315d50f39ae173e8
Ilyas <issatayev.ilyas@gmail.com>
2019-01-26 02:18:24 +0100

    Added repair things

--- GameJamHome/Assets/_Scripts/SectionDestroyedState.cs
+++ GameJamHome/Assets/_Scripts/SectionDestroyedState.cs
@@ -8,6 +8,7 @@ public class SectionDestroyedState : AbstractState<SectionFSM>, IPointerClickHan
     private BoxCollider2D m_collider2D;
     private MeshRenderer m_meshRenderer;
     private SectionData m_sectionData;
+    private SectionFSM m_sectionFSM;
 
     public void Start()
     {
@@ -18,12 +19,12 @@ public class SectionDestroyedState : AbstractState<SectionFSM>, IPointerClickHan
     {
         base.Enter(pAgent);
 
-        if (!m_sectionData) m_sectionData = GetComponent<SectionData>();
-        if (!m_collider2D) m_collider2D = GetComponent<BoxCollider2D>();
+        if (!m_sectionData)  m_sectionData = GetComponent<SectionData>();
+        if (!m_collider2D)   m_collider2D = GetComponent<BoxCollider2D>();
         if (!m_meshRenderer) m_meshRenderer = GetComponent<MeshRenderer>();
+        if (!m_sectionFSM)   m_sectionFSM = GetComponent<SectionFSM>();
 
         m_meshRenderer.material = m_sectionData.m_destroyedMaterial;
-
         DisableObject();
     }
 
@@ -35,11 +36,14 @@ public class SectionDestroyedState : AbstractState<SectionFSM>, IPointerClickHan
     public override void Exit(IAgent pAgent)
     {
         base.Exit(pAgent);
+        m_meshRenderer.material = m_sectionData.m_initialMaterial;
+        DisableObject(false);
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
         Debug.Log("CLICK");
+        m_sectionFSM.fsm.ChangeState<SectionCompleteState>();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -61,6 +65,7 @@ public class SectionDestroyedState : AbstractState<SectionFSM>, IPointerClickHan
     void HighlightObject(bool highlight)
     {
         //m_meshRenderer.enabled = highlight;
+        
 
         if (highlight) m_meshRenderer.material = m_sectionData.m_highlightMaterial;
         else           m_meshRenderer.material = m_sectionData.m_destroyedMaterial;
