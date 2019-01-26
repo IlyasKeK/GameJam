43a3928ea475289a15cea2840f9a8ab45bf19288
Ilyas <issatayev.ilyas@gmail.com>
2019-01-26 21:34:59 +0100

    minor

---
+++ GameJamHome/Assets/_Scripts/Explosion.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour {
+    [SerializeField]
+    private float m_explosionRadius = 5;
+    [SerializeField]
+    private int m_damage = 100;
+
+    Animator m_animator;
+
+    public bool isActivated = false;
+
+	void Start () {
+        m_animator = GetComponent<Animator>();
+        m_animator.speed = 0;
+
+    }
+
+    public void Explode()
+    {
+        Collider2D[] colliders=Physics2D.OverlapCircleAll(transform.position, m_explosionRadius);
+        Debug.Log("Colliders in explosion radius: "+colliders.Length);
+
+        m_animator.speed = 1;
+        
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.GetComponent<SectionCompleteState>())
+            {
+                collider.GetComponent<SectionCompleteState>().DealDamage(m_damage);
+                Debug.Log(">>> Found destroyable section and destroyed it");
+            }
+        }
+    }
+	
+	void Update () {
+        if (!isActivated)
+        {
+            isActivated = true;
+            Explode();
+        }
+	}
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, m_explosionRadius);
+    }
+}
---
+++ GameJamHome/Assets/_Scripts/SectionCollider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SectionCollider : MonoBehaviour {
+
+    public Action<Collision2D> onCollisionEnter;
+
+    [HideInInspector]
+    public BoxCollider2D boxCollider2D;
+    
+
+	void Start ()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (null != onCollisionEnter) onCollisionEnter(collision);
+    }
+}
