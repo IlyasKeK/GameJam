3985bb34839bc4cff948417a425c717b1f627ab3
Ilyas <issatayev.ilyas@gmail.com>
2019-01-26 12:14:32 +0100

    scripts done

--- GameJamHome/Assets/_Scripts/BoilerBehaviour.cs
+++ GameJamHome/Assets/_Scripts/BoilerBehaviour.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class BoilerBehaviour : MonoBehaviour
 {
+    public enum Player { ONE, TWO }
+    public Player player;
+
     public void DestroyBoiler()
     {
         Debug.Log("BOILER IS DESTROYED");
+        GameManager.Instance().ResolveEndGame(this);
     }
 }
