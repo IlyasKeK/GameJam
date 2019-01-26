7a115b74a7096fa6ff33f13c7c6e91bbdedcdd10
Ilyas <issatayev.ilyas@gmail.com>
2019-01-26 16:50:38 +0100

    Added ResourceManager and implemented in Repairing things

---
+++ GameJamHome/Assets/_Scripts/ResourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceManager : MonoBehaviour
+{
+    public int initialResources = 10;
+    public int repairCost = 1;
+    public int upgradeCannonCost = 5;
+
+    private static ResourceManager m_instance;
+
+    void Start ()
+    {
+        m_instance = this;
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    public static ResourceManager Instance()
+    {
+        return m_instance;
+    }
+}
