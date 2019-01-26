0cd0781b89e04b7eb770f13a16574ad1ba060031
Ilyas <issatayev.ilyas@gmail.com>
2019-01-26 01:26:15 +0100

    Added Material switching when mouse over,click object

---
+++ GameJamHome/Assets/_Scripts/SectionData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SectionData : MonoBehaviour
+{
+    public Material m_initialMaterial;
+    public Material m_highlightMaterial;
+    public Material m_destroyedMaterial;
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+}
--- GameJamHome/Assets/_Scripts/SectionDestroyedState.cs
+++ GameJamHome/Assets/_Scripts/SectionDestroyedState.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class SectionDestroyedState : AbstractState<SectionFSM>
+public class SectionDestroyedState : AbstractState<SectionFSM>, IPointerClickHandler,IPointerEnterHandler,IPointerExitHandler
 {
     private BoxCollider2D m_collider2D;
     private MeshRenderer m_meshRenderer;
+    private SectionData m_sectionData;
 
     public void Start()
     {
@@ -15,6 +17,13 @@ public class SectionDestroyedState : AbstractState<SectionFSM>
     public override void Enter(IAgent pAgent)
     {
         base.Enter(pAgent);
+
+        if (!m_sectionData) m_sectionData = GetComponent<SectionData>();
+        if (!m_collider2D) m_collider2D = GetComponent<BoxCollider2D>();
+        if (!m_meshRenderer) m_meshRenderer = GetComponent<MeshRenderer>();
+
+        m_meshRenderer.material = m_sectionData.m_destroyedMaterial;
+
         DisableObject();
     }
 
@@ -28,13 +37,32 @@ public class SectionDestroyedState : AbstractState<SectionFSM>
         base.Exit(pAgent);
     }
 
-    void DisableObject()
+    public void OnPointerClick(PointerEventData eventData)
     {
+        Debug.Log("CLICK");
+    }
 
-        if(!m_collider2D)   m_collider2D = GetComponent<BoxCollider2D>();
-        if(!m_meshRenderer) m_meshRenderer = GetComponent<MeshRenderer>();
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        HighlightObject(true);
+    }
 
-        m_collider2D.enabled = false;
-        m_meshRenderer.enabled = false;
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        HighlightObject(false);
+    }
+
+    void DisableObject(bool disable=true)
+    {
+        m_collider2D.isTrigger =  disable;
+        //m_meshRenderer.enabled = !disable;
+    }
+
+    void HighlightObject(bool highlight)
+    {
+        //m_meshRenderer.enabled = highlight;
+
+        if (highlight) m_meshRenderer.material = m_sectionData.m_highlightMaterial;
+        else           m_meshRenderer.material = m_sectionData.m_destroyedMaterial;
     }
 }
